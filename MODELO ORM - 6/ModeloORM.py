8b0ec3dfef337916a9f06734da9213d4ebdf36d5
Oscar <151192670+Oscarroes@users.noreply.github.com>
2023-12-10 11:49:54 +0100

    Comentarios al código y pruebas de funcionamiento

--- MODELO ORM - 6/ModeloORM.py
+++ MODELO ORM - 6/ModeloORM.py
@@ -7,7 +7,7 @@ import json
 personas = []
 
 class Persona:
-
+    #CONSTRUCTOR DE LAS ENTIDADES PERSONA
     def __init__(self):
         self.posx = random.randint(0,1024)
         self.posy = random.randint(0,600)
@@ -16,6 +16,7 @@ class Persona:
         self.color = ""
         self.velocidad = 50
         self.entidad = ""
+    #DIBUJA LAS ENTIDADES = LES DAMOS COLOR Y UNA COORDENADA DE POSICIÓN
     def dibuja(self):
         self.entidad = lienzo.create_oval(
             self.posx-self.radio/2,
@@ -23,6 +24,7 @@ class Persona:
             self.posx+self.radio/2,
             self.posy+self.radio/2,
             fill=self.color)
+    #DEFINE EL MOVIMIENTO DE LAS ENTIDADES Y LOS LÍMITES DEL CANVAS PARA QUE NO ESCAPEN
     def mueve(self):
         self.colisiona()
         self.cambiaColor()
@@ -34,6 +36,7 @@ class Persona:
     def colisiona(self):
         if self.posx <0 or self.posx > 1024 or self.posy < 0 or self.posy > 600:
             self.direccion += math.pi
+    #CAMBIA EL COLOR DE LAS ENTIDADES DEPENDIENDO DEL CUADRANTE CARTESIANO QUE OCUPEN
     def cambiaColor(self):
         if self.posx < 512 and self.posy > 300:
             self.color = "blue"
@@ -47,6 +50,24 @@ class Persona:
         elif self.posx > 512 and self.posy < 300:
             self.color = "red"
             lienzo.itemconfig(self.entidad, fill=self.color)
+    #CAMBIA LA VELOCIDAD DE LAS ENTIDADES SEGÚN SELECCINEMOS
+    def cambiaVelocidad(event):
+        if ajustaVelocidad.get() == 'Muy lento':
+            for persona in personas:
+                persona.velocidad = 400
+        elif ajustaVelocidad.get() == 'Lento':
+            for persona in personas:
+                persona.velocidad = 125
+        elif ajustaVelocidad.get() == 'Medio':
+            for persona in personas:
+                persona.velocidad = 50
+        elif ajustaVelocidad.get() == 'Rapido':
+            for persona in personas:
+                persona.velocidad = 20
+        elif ajustaVelocidad.get() == 'Muy rapido':
+            for persona in personas:
+                persona.velocidad = 5
+    #GUARDA EL ESTADO DE LAS ENTIDADES
     def guardarPersonas():
         print("Guardo a los jugadores")
         cadena = json.dumps([vars(persona) for persona in personas])
@@ -54,22 +75,6 @@ class Persona:
         archivo = open("jugadores.json",'w')
         archivo.write(cadena)
         archivo.close()
-    def cambiaVelocidad(event):
-        if ajustaVelocidad.get() == 'Muy lento':
-            for persona in personas:
-                persona.velocidad = 500
-        elif ajustaVelocidad.get() == 'Lento':
-            for persona in personas:
-                persona.velocidad = 150
-        elif ajustaVelocidad.get() == 'Medio':
-            for persona in personas:
-                persona.velocidad = 75
-        elif ajustaVelocidad.get() == 'Rapido':
-            for persona in personas:
-                persona.velocidad = 20
-        elif ajustaVelocidad.get() == 'Muy rapido':
-            for persona in personas:
-                persona.velocidad = 5
 
         
 #CREAMOS LA VENTANA
@@ -82,7 +87,6 @@ lienzo = tk.Canvas(raiz,width=1024,height=600)
 lienzo.pack()
 
 #COMBOBOX PARA AJUSTAR LA VELOCIDAD
-
 ajustaVelocidad = ttk.Combobox(raiz,values=['Muy lento','Lento','Medio','Rapido','Muy rapido'])
 ajustaVelocidad.pack(padx=10,pady=10)
 ajustaVelocidad.bind('<<ComboboxSelected>>', Persona.cambiaVelocidad)
